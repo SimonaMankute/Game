e9a42cd72da3c44199dc431a45ed05c741d93904
SimonaMankute <63453201+SimonaMankute@users.noreply.github.com>
2021-03-09 23:54:10 +0200

    Initial commit

---
+++ game.js
@@ -0,0 +1,11 @@
+
+  function perskaityk() {
+    var text = document.getElementById("skaityti");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+/*var person = prompt("Įvesk savo vardą");*/
