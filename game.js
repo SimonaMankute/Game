185da28c59d5411dfcd33d5d0329ab85ac13ff52
SimonaMankute <63453201+SimonaMankute@users.noreply.github.com>
2021-03-11 22:56:23 +0200

    papildymas

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-  function perskaityk() {
+function perskaityk() {
     var text = document.getElementById("skaityti");
     if (text.style.display === "none") {
       text.style.display = "block";
@@ -7,5 +7,17 @@
       text.style.display = "none";
     }
   }
+
+function zaisk() {
+    var text = document.getElementById("zaidimas");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+
+  
 
 /*var person = prompt("Įvesk savo vardą");*/
