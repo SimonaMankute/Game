190910355f640ca8c080df7fc576a96b473b56ab
SimonaMankute <63453201+SimonaMankute@users.noreply.github.com>
2021-03-14 09:43:46 +0200

    update 03.13

--- game.js
+++ game.js
@@ -17,7 +17,44 @@ function zaisk() {
     }
   }
 
+  function iveskVarda1() {
+    var person = prompt("Įvesk savo vardą");
+    if (person == '') {
+      alert("Tai ne vardas. Įveskite savo vardą.");
+      return false;
+    } else {
+        let x = person;
+        document.getElementById("vardoLaukas1").value=x;
+    }
+  }
 
-  
+function iveskVarda2() {
+  var person = prompt("Įvesk savo vardą");
+  if (person == '') {
+    alert("Tai ne vardas. Įveskite savo vardą.");
+    return false;
+  } else {
+      let x = person;
+      document.getElementById("vardoLaukas2").value=x;
+  }
+}
 
-/*var person = prompt("Įvesk savo vardą");*/
+function iveskVarda3() {
+  var person = prompt("Įvesk savo vardą");
+  if (person == '') {
+    alert("Tai ne vardas. Įveskite savo vardą.");
+    return false;
+  } else {
+      let x = person;
+      document.getElementById("vardoLaukas3").value=x;
+      alert(`Gražus vardas, ${x}! Gali įrašyti vardus ir kitiems žaidėjams!`);
+      return true;
+  }
+}
+
+window.onload = function() {
+  document.getElementById("vardoLaukas1").value="";
+  document.getElementById("vardoLaukas2").value="";
+  document.getElementById("vardoLaukas3").value="";
+} 
+
